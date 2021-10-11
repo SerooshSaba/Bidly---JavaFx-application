76bd48d44858aff1fa17a720130b8947405cca1e
SerooshSaba <serooshsaba@gmail.com>
2021-10-11 08:00:16 +0200

    Worked futher on the bidder controller and view

--- Bidly/database.sql
+++ Bidly/database.sql
@@ -1,6 +1,5 @@
 drop table stores;
 drop table antiqes;
-drop table customers;
 drop table bids;
 
 PRAGMA foreign_keys = ON;
@@ -17,25 +16,51 @@ CREATE TABLE antiqes(
                         name        TEXT NOT NULL,
                         description TEXT NOT NULL,
                         pic_url     TEXT NOT NULL,
-                        price       INTEGER NOT NULL,
+                        price       INTEGER NOT NULL, -- Change to start_bid
                         store_id    INTEGER NOT NULL,
 
                         FOREIGN KEY (store_id) REFERENCES stores(store_id) ON UPDATE CASCADE ON DELETE CASCADE
-);
-
-CREATE TABLE customers(
-                          customer_id INTEGER NOT NULL PRIMARY KEY,
-                          name        TEXT NOT NULL,
-                          card_info   TEXT NOT NULL
 );
 
 CREATE TABLE bids (
                       bid_id      INTEGER NOT NULL PRIMARY KEY,
                       amount      INTEGER NOT NULL,
                       antiqe_id   INTEGER NOT NULL,
-                      customer_id INTEGER NOT NULL,
-                      FOREIGN KEY (antiqe_id) REFERENCES antiqes(antiqe_id) ON UPDATE CASCADE ON DELETE CASCADE,
-                      FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON UPDATE CASCADE ON DELETE CASCADE
+                      FOREIGN KEY (antiqe_id) REFERENCES antiqes(antiqe_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
--- INSERT
+-- INSERTS
+
+INSERT INTO stores VALUES
+( NULL, 'John''s store' ),
+( NULL, 'Tommy''s store' );
+
+INSERT INTO antiqes VALUES
+( NULL, 'Shoe', 'Rare shoe from japan', 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/464e8d65-3a82-472a-aa2c-de53b2dfe7f2/wearallday-shoe-HJSFrd.png', 1000, 1 ),
+( NULL, 'Table', 'Victorian era table', 'https://vipp.com/sites/default/files/vipp_packs_large_table_darkoak_03_lowres.jpg', 5250, 1 ),
+( NULL, 'Dragon statue', 'Statue from ancient japan', 'https://img.fruugo.com/product/6/99/168070996_max.jpg', 12500, 1 ),
+( NULL, 'Tsar egg', 'Diamond encrusted russian egg', 'https://i5.walmartimages.com/asr/5f6e98a2-0e4f-4621-a80b-4cb8260e7e14.b5a3e5da29c3869030faf670df5443f9.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF', 45000, 1 ),
+( NULL, 'American classic', 'Classic from 1960', 'https://cdn2.lamag.com/wp-content/uploads/sites/6/2021/09/Black-Car_RezUp-slight_SILO_1500x1000px-1068x712.jpg', 250000, 1 ),
+( NULL, 'Revolver', 'Victorian era revolver', 'https://cdn.shopify.com/s/files/1/1524/1342/products/ON9889__01.jpg?v=1604197881', 5000, 2 ),
+( NULL, 'Hat', 'Hat from 1870''s', 'https://gambleandgunn.com/wp-content/uploads/2019/05/DSC_5244-560x560.png', 8025, 2 ),
+( NULL, 'Brush', 'Brush from 1750''s england', 'https://gambleandgunn.com/wp-content/uploads/2019/05/DSC_5424-560x560.png', 5000, 2 ),
+( NULL, 'Lamp', 'Old school lamp', 'https://cdn5.mystore4.no/thumb/333_500/hbygg/97102_Light___Living_Kalym_lampefot_antique_bronze_1.jpg', 1000, 2 ),
+( NULL, 'Clock', 'Old clock from germany', 'https://litb-cgis.rightinthebox.com/images/640x640/201407/lkaaed1405388815503.jpg?fmt=webp&v=1', 6522, 2 );
+
+-- IMPORTANT
+
+SELECT
+antiqes.antiqe_id,
+antiqes.name,
+antiqes.description,
+antiqes.pic_url,
+antiqes.price,
+MAX(bids.amount) AS last_bid,
+stores.name AS storename
+FROM antiqes
+LEFT OUTER JOIN bids ON bids.antiqe_id = antiqes.antiqe_id
+INNER JOIN stores ON antiqes.store_id = stores.store_id
+
+WHERE antiqes.antiqe_id = 3
+
+GROUP BY antiqes.antiqe_id;
--- Bidly/src/main/java/com/bidly/bidly/BidderController.java
+++ Bidly/src/main/java/com/bidly/bidly/BidderController.java
@@ -1,8 +1,12 @@
 package com.bidly.bidly;
 
 import Utility.Database;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -11,8 +15,12 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.EventListener;
 
 public class BidderController {
 
@@ -32,19 +40,17 @@ public class BidderController {
     Database database = new Database();
 
     public void initialize() throws SQLException {
-
         // Load logo
         Image logoImage = new Image(String.valueOf(Application.class.getResource("/images/logo.PNG")));
         logo.setPreserveRatio(true);
         logo.setFitWidth(75);
         logo.setTranslateX(-30);
         logo.setImage(logoImage);
-
         // Get all products on platform
         String query_string = "SELECT antiqes.antiqe_id, antiqes.name, antiqes.description, antiqes.pic_url, antiqes.price, stores.name AS storename FROM antiqes INNER JOIN stores on antiqes.store_id = stores.store_id";
         database.statement(query_string);
         ResultSet result = database.getResult();
-
+        // Render products
         HBox row = createListRow();
         int product = 1;
         while ( result.next() ) {
@@ -62,32 +68,89 @@ public class BidderController {
         }
         productListingContainer.getChildren().add(row);
         database.close();
+    }
 
+    @FXML
+    protected void logoutClick(ActionEvent event) throws IOException {
+        FXMLLoader root = new FXMLLoader(Application.class.getResource("authentication.fxml"));
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root.load(), 750, 500);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void bidClick( int antiqe_id, int start_bid_price, int bid_amount, Label bid_message ) throws SQLException {
+        // Find if there are bids for this product
+        database.statement("SELECT COUNT(*) AS bids FROM bids WHERE antiqe_id = ? ");
+        database.passInt(antiqe_id);
+        ResultSet result = database.getResult();
+
+        int bids = Integer.parseInt(result.getString("bids"));
+        database.close();
+
+        if ( bids == 0 ) { // If there are no bids
+            // If bid is higher than the start bid price
+            if ( bid_amount > start_bid_price ) {
+                database.statement("INSERT INTO bids VALUES (NULL,?,?)");
+                database.passInt(bid_amount);
+                database.passInt(antiqe_id);
+                database.execute();
+                database.close();
+                bid_message.setText("Bid success!");
+                bid_message.setStyle("-fx-font-size:8.5;-fx-text-fill:green");
+                bid_message.setVisible(true);
+            } else {
+                bid_message.setText("Bid must be higher than previous!");
+                bid_message.setStyle("-fx-font-size:8.5;-fx-text-fill:red");
+                bid_message.setVisible(true);
+            }
+        } else { // If there are bids
+
+            // Find the highest bid for product
+            database.statement("SELECT MAX(amount) as maxbid FROM bids WHERE antiqe_id = ?");
+            database.passInt(antiqe_id);
+            int highest_bid = database.getResult().getInt("maxbid");
+            database.close();
+            if ( bid_amount > highest_bid ) {
+                database.statement("INSERT INTO bids VALUES (NULL,?,?)");
+                database.passInt(bid_amount);
+                database.passInt(antiqe_id);
+                database.execute();
+                database.close();
+                bid_message.setText("Bid success!");
+                bid_message.setStyle("-fx-font-size:8.5;-fx-text-fill:green");
+                bid_message.setVisible(true);
+            }
+        }
     }
 
     private HBox createListRow() {
         HBox row = new HBox();
         row.setAlignment(Pos.CENTER);
+        row.setStyle("-fx-background-color:white");
         return row;
     }
 
-    private VBox createProductView( int id, String productname, String desc, String picurl, int price, String store_name ) {
+    private VBox createProductView( int product_id, String productname, String desc, String picurl, int price, String store_name ) {
 
         // Create main container
         VBox container = new VBox();
-        container.setStyle("-fx-border-color:rgba(0,0,0,0.1);-fx-border-radius:10");
+        container.setStyle("-fx-border-color:rgba(0,0,0,0.2);-fx-border-radius:10;-fx-background-color:rgba(0,0,0,0.1)");
         container.setSpacing(5);
-        container.setMinHeight(200);
+        container.setMinHeight(250);
         container.setMinWidth(180);
         container.setAlignment(Pos.CENTER);
 
-        // Label for name
+        // Label for store name
+        Label storename = new Label(store_name);
+        storename.setStyle("-fx-font-size:10");
+
+        // Label for product name
         Label name = new Label( productname );
         name.setStyle("-fx-font-size:12.5;-fx-font-weight:bold");
 
         // ImageView for image of product
-        //Image image_object = new Image(String.valueOf(Application.class.getResource(picurl)),true);
-        Image image_object = new Image(String.valueOf(Application.class.getResource("/images/box.PNG")),true);
+        Image image_object = new Image(picurl,true);
         ImageView image = new ImageView(image_object);
         image.setFitHeight(75);
         image.setFitWidth(75);
@@ -108,16 +171,32 @@ public class BidderController {
         bid_amount.setStyle("-fx-text-fill:green");
         bid_data_container.getChildren().addAll(bid_text,bid_amount);
 
-        // Bid input 3 Bid Button
+        // Message label
+        Label message_output = new Label();
+        message_output.setStyle("-fx-font-size:8.5");
+        message_output.setVisible(false);
+
+        // Bid input & Bid Button
         HBox bid_input_container = new HBox();
         bid_input_container.setAlignment(Pos.CENTER);
         TextField bid_field = new TextField();
         Button bid_button = new Button("bid");
         bid_field.setPromptText("$$$");
         bid_field.setMaxWidth(75);
-        bid_input_container.getChildren().addAll(bid_field,bid_button);
 
-        container.getChildren().addAll( name, image, description, bid_data_container, bid_input_container );
+        bid_button.setOnAction(e -> {
+            try {
+                int bid_amount_converted = Integer.parseInt( bid_field.getText() );
+                this.bidClick( product_id, price, bid_amount_converted, message_output );
+            } catch (NumberFormatException | SQLException exception ) {
+                message_output.setVisible(true);
+                message_output.setText("Input a number");
+                message_output.setStyle("-fx-font-size:8.5;-fx-text-fill:red");
+            }
+        });
+
+        bid_input_container.getChildren().addAll(bid_field,bid_button);
+        container.getChildren().addAll( storename, name, image, description, bid_data_container, message_output, bid_input_container );
         return container;
     }
 
