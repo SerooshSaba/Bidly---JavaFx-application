7694a42be20b1c8252cec99609af4fe5da7f0b0b
H4ckerM4nn <sebaste@hiof.no>
2021-11-04 13:29:03 +0100

    Tests if antiqes get values

--- Bidly/src/main/test/java/BidlyCore/AntiqeTest.java
+++ Bidly/src/main/test/java/BidlyCore/AntiqeTest.java
@@ -10,4 +10,18 @@ public class AntiqeTest {
         Antiqe antiqe = new Antiqe("Object", "text", "url", 1000, 1 );
         Assertions.assertEquals(0, antiqe.getAntiqe_id());
     }
+
+    @Test
+    void antique_table_fill_value() {
+        Antiqe antiqe = new Antiqe(1, "text", "text", "url", 100, 100, 1, "name"  );
+        Assertions.assertEquals(antiqe.getAntiqe_id(), 1);
+        Assertions.assertEquals(antiqe.getName(), "text");
+        Assertions.assertEquals(antiqe.getDescription(), "text");
+        Assertions.assertEquals(antiqe.getPic_url(), "url");
+        Assertions.assertEquals(antiqe.getPrice(), 100);
+        Assertions.assertEquals(antiqe.getLast_bid_price(), 100);
+        Assertions.assertEquals(antiqe.getStore_id(), 1);
+        Assertions.assertEquals(antiqe.getStoreName(), "name");
+    }
+
 }
