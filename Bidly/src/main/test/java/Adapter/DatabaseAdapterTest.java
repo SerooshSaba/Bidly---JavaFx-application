0e1bcd34f4994a338a784c47438365f932d47607
H4ckerM4nn <sebaste@hiof.no>
2021-11-08 15:32:24 +0100

    Added test for get amount of bids

--- Bidly/src/main/test/java/Adapter/DatabaseAdapterTest.java
+++ Bidly/src/main/test/java/Adapter/DatabaseAdapterTest.java
@@ -3,25 +3,40 @@ package Adapter;
 import BidlyCore.Antiqe;
 import org.junit.jupiter.api.*;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 class DatabaseAdapterTest {
 
     public DatabaseAdapter databaseAdapter = new DatabaseAdapter("testing.sqlite");
 
+    @BeforeEach
+    void insertAntiqe() throws SQLException {
+        databaseAdapter.insertAntiqe(new Antiqe("Object", "text", "url", 1000, 1 ));
+        databaseAdapter.insertBid(1001, 1);
+    }
+
     @AfterEach
     void clearTables() throws SQLException {
         databaseAdapter.runQuery("DELETE FROM Antiqes;DELETE FROM bids;DELETE FROM stores;");
+        databaseAdapter.runQuery("DELETE FROM bids");
+
     }
 
     @Test
-    void test_insertAntique_and_getAllProducts() throws SQLException {
-        databaseAdapter.insertAntiqe(new Antiqe("Object", "text", "url", 1000, 1 ));
+    void test_getAllProducts() throws SQLException {
         Antiqe returned_antiqe = databaseAdapter.getStoreProducts(1).get(0);
         Assertions.assertEquals("Object",   returned_antiqe.getName());
         Assertions.assertEquals("text",     returned_antiqe.getDescription());
         Assertions.assertEquals("url",      returned_antiqe.getPic_url());
         Assertions.assertEquals(1000,       returned_antiqe.getPrice());
-        Assertions.assertEquals(1,          returned_antiqe.getStore_id() );
+        Assertions.assertEquals(1,          returned_antiqe.getStore_id());
     }
+
+    @Test
+    void test_getAmountOfBidsForAntiqe() throws SQLException {
+        int bids = databaseAdapter.getAmountOfBidsForAntiqe(1);
+        Assertions.assertEquals(1, bids);
+    }
+
 
 }
