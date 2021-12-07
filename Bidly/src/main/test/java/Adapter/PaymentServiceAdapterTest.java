2cc25ac18c7796e58a68bb11971f4417efa8c618
H4ckerM4nn <sebaste@hiof.no>
2021-12-07 18:00:47 +0100

    fikk med krav i PSA

--- Bidly/src/main/test/java/Adapter/PaymentServiceAdapterTest.java
+++ Bidly/src/main/test/java/Adapter/PaymentServiceAdapterTest.java
@@ -8,13 +8,13 @@ public class PaymentServiceAdapterTest {
     PaymentServiceAdapter paymentServiceAdapter = new PaymentServiceAdapter();
 
     @Test
-    @DisplayName("Correct payment info test")
+    @DisplayName("Correct payment info test / Krav 7.1.10")
     void test_processPayment_with_correct_info() {
         assertTrue( paymentServiceAdapter.processPayment("1234 1234 1234 1234","01/09","321") );
     }
 
     @Test
-    @DisplayName("Incorrect payment info test")
+    @DisplayName("Incorrect payment info test / Krav 7.1.10")
     void test_processPayment_with_incorrect_info() {
         assertFalse( paymentServiceAdapter.processPayment("4233 1554 2654 2345","02/12","897") );
     }
